ca2ef3b13abd34342bb9a2d8690ef3785f4b57fd
Quentin Kleinert <q.kleinert@sciospec.de>
2025-05-19 08:04:20 +0200

    Added security

---
+++ src/main_script.py
@@ -0,0 +1,33 @@
+import check_User_Input as input_user
+
+from src.ISX3 import ISX3
+
+
+try:
+    isx3 = ISX3(n_el=4)
+    isx3.connect_device_FS(port="COM3")  # change to com port if necessary
+
+    isx3.set_fs_settings(
+        measurement_mode=2,
+        measurement_channel="Main Port",
+        current_measurement_range="10mA",
+        voltage_measurement_range="autoranging"
+    )
+
+    isx3.set_setup(
+        start_frequency="1kHz",
+        end_frequency="10MHz",
+        count=2,
+        scale="log",
+        precision=1.0,
+        amplitude="100mV",
+        excitation_type="voltage"
+    )
+
+    results = isx3.start_measurement(spectres=2)
+
+    print(results)
+
+except AttributeError:
+    print("Device is not connected.")
+
